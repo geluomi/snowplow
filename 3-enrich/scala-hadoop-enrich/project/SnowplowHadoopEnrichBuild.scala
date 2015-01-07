b660d7f8b8b3fb2d7e62b49a81e51fd17581895d
Fred Blundun <fred@snowplowanalytics.com>
2015-01-07 17:49:54 +0000

    Scala Hadoop Enrich: added new dependencies for Thrift job

--- 3-enrich/scala-hadoop-enrich/project/SnowplowHadoopEnrichBuild.scala
+++ 3-enrich/scala-hadoop-enrich/project/SnowplowHadoopEnrichBuild.scala
@@ -33,12 +33,15 @@ object SnowplowHadoopEnrichBuild extends Build {
         // Scala
         Libraries.scaldingCore,
         Libraries.scaldingArgs,
+        Libraries.scaldingCommons,
+        Libraries.scaldingJson,
         Libraries.scalaz7,
         Libraries.snowplowRawEvent,
         Libraries.commonEnrich,
         // Scala (test only)
         Libraries.specs2,
         Libraries.scalazSpecs2,
+        Libraries.compress,
         Libraries.commonsCodec
       )
     )
